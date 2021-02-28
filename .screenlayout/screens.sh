5a2c32ffc15cb38c48b35dd611a7f689bef4d00a
TomokoK <TomokoK@users.noreply.github.com>
2021-02-28 18:57:28 -0500

    updated for winter 2021

--- .screenlayout/screens.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-0 --mode 2560x1080 --pos 6200x0 --rotate right --output DP-0 --mode 2560x1080 --pos 0x0 --rotate left --output DP-1 --off --output DP-2 --primary --mode 5120x1440 --pos 1080x1120 --rotate normal --rate 120 --output DP-3 --off --output DP-4 --off --output DP-5 --off
